89a470f1169ba327f506743da4a1edda19434739
vanozi <williewellee@gmail.com>
2026-06-08 14:59:09 +0200

    aanpasseing datajob

--- deploy/run-nightly-datajobs-klauwscore.sh
+++ deploy/run-nightly-datajobs-klauwscore.sh
@@ -3,8 +3,5 @@ set -eu
 
 cd "$(dirname "$0")/.."
 
-COMPOSE_FILES="-f docker-compose.yml"
-ENV_FILE="deploy/dashboard.env"
-
-docker compose --env-file "$ENV_FILE" $COMPOSE_FILES --profile jobs run --rm datajob-klauwscore
+docker compose -f docker-compose.yml --profile jobs run --rm datajob-klauwscore
 
